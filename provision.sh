972c081709c6a7cb2960314c65954049670e5533
amir <amiri.amir30@yahoo.com>
2020-11-02 15:45:00 +0330

    envs var updated - dependencies installations

--- provision.sh
+++ provision.sh
@@ -48,6 +48,98 @@ ln -s /etc/nginx/sites-available/$PROJECT_NAME.conf /etc/nginx/sites-enabled/$PR
 nginx -t
 service nginx restart
 fi
+# creating database
+echo "creating main database ..."
+echo "enter [user] [host] [port](optional): "
+
+read DB_HOST_USER HOST PORT
+
+if [ -z "$PORT" ]; then
+	PORT=5432
+fi
+
+DB_USER_CREATE=${PROJECT_NAME}_user$(awk -v min=1 -v max=1000 'BEGIN{srand(); print int(min+rand()*(max-min+1))}')
+DB_USER_CREATE_PASSWORD=$(randomPassword)
+psql -U "$DB_HOST_USER" -h "$HOST" -p "$PORT" -W <<EOF
+CREATE DATABASE $PROJECT_NAME WITH ENCODING 'UTF8';
+CREATE USER $DB_USER_CREATE WITH PASSWORD '$DB_USER_CREATE_PASSWORD';
+GRANT ALL PRIVILEGES ON DATABASE $PROJECT_NAME TO $DB_USER_CREATE;
+
+
+EOF
+echo "USER OF THE DATABASE $PROJECT_NAME IS $DB_USER_CREATE WITH PASSWORD $DB_USER_CREATE_PASSWORD"
+echo "DB_USER='$DB_USER_CREATE'
+DB_PASSWORD='$DB_USER_CREATE_PASSWORD'
+DB_NAME='$PROJECT_NAME'
+DB_HOST='$HOST'
+DB_PORT='$PORT'" >> $PROJECT_DIR/project/.env
+echo "done"
+
+
+#set supervisor
+echo "Do you wish to set supervisor for $PROJECT_NAME ? (y/n)"
+read spyn
+if [[ ($spyn == "y" || $spyn == "") ]]; then
+	echo "setting supervisor configurations ..."
+	echo "Do you have beat for $PROJECT_NAME ? (y/n)"
+	read btyn
+	echo "setting worker and beat configurations ..."
+	if [[ ($btyn == "y" || $btyn == "") ]]; then
+		echo "[group:$PROJECT_NAME]
+programs=$PROJECT_NAME-beat,$PROJECT_NAME-worker" >> $PROJECT_DIR/confs/supervisor.conf
+  else
+    echo "[group:$PROJECT_NAME]
+programs=$PROJECT_NAME-worker" >> $PROJECT_DIR/confs/supervisor.conf
+	fi
+
+	sed -e "s/\$PROJECT_NAME/$PROJECT_NAME/g" -e "s/\$OWNER_USER/$OWNER_USER/g"  supervisor-worker-template.conf >> $PROJECT_DIR/confs/supervisor.conf
+
+	if [[ ($btyn == "y" || $btyn == "") ]]; then
+		sed -e "s/\$PROJECT_NAME/$PROJECT_NAME/g" -e "s/\$OWNER_USER/$OWNER_USER/g"  supervisor-beat-template.conf >> $PROJECT_DIR/confs/supervisor.conf
+	fi
+
+	ln -s $PROJECT_DIR/confs/supervisor.conf /etc/supervisor/conf.d/$PROJECT_NAME.conf
+
+	supervisorctl reread
+	supervisorctl update
+
+fi
+
+if [[ ($spyn == "y" || $spyn == "") ]]; then
+	echo "creating broker vhost ..."
+	echo "enter [username] [password] [host] [port](optional): "
+	read RBMQ_USERNAME RBMQ_PASSWORD RBMQ_HOST RBMQ_PORT
+	if [ -z "$RBMQ_PORT" ]; then
+	RBMQ_PORT=15672
+  fi
+
+	RBMQ_USER_CREATE=${PROJECT_NAME}_user$(awk -v min=1 -v max=1000 'BEGIN{srand(); print int(min+rand()*(max-min+1))}')
+	RBMQ_USER_CREATE_PASSWORD=$(randomPassword)
+	curl -u "$RBMQ_USERNAME":"$RBMQ_PASSWORD" -X PUT http://"$RBMQ_HOST":"$RBMQ_PORT"/api/vhosts/"$PROJECT_NAME"
+
+
+	curl -i -u "$RBMQ_USERNAME":"$RBMQ_PASSWORD" -H "content-type:application/json"     -XPUT -d'{"password":"'$RBMQ_USER_CREATE_PASSWORD'","tags":"monitoring"}'     http://"$RBMQ_HOST":"$RBMQ_PORT"/api/users/"$RBMQ_USER_CREATE"
+        curl -i -u "$RBMQ_USERNAME":"$RBMQ_PASSWORD" -H "content-type:application/json"     -XPUT -d'{"configure":".*","write":".*","read":".*"}'     http://"$RBMQ_HOST":"$RBMQ_PORT"/api/permissions/"$PROJECT_NAME"/"$RBMQ_USER_CREATE"
+
+	echo "USER OF THE RBMQ $PROJECT_NAME IS $RBMQ_USER_CREATE WITH PASSWORD $RBMQ_USER_CREATE_PASSWORD"
+
+	echo "CELERY_USER='$RBMQ_USER_CREATE'
+CELERY_PASS='$RBMQ_USER_CREATE_PASSWORD'
+CELERY_HOST='$RBMQ_HOST:$RBMQ_PORT/$PROJECT_NAME'" >> $PROJECT_DIR/project/.env
+fi
+
+# installing dependencies and migrations
+echo "DEBUG=False
+DEVEL=False
+ALLOWED_HOSTS='*'
+SECRET_KEY=''" >> $PROJECT_DIR/project/.env
+
+cd $PROJECT_DIR/project
+pip install -r requirements.txt
+python manage.py makemigrations
+python manage.py migrate
+python manage.py collectstatic --noinput
+python manage.py loaddata fixtures/*
 
 #set uwsgi
 echo "Do you wish to set uwsgi for $PROJECT_NAME ? (y/n)"
@@ -68,87 +160,10 @@ fi
 sed -e "s/\$PROJECT_NAME/$PROJECT_NAME/g" -e "s/\$OWNER_USER/$OWNER_USER/g" -e "s/\$WORKER_PROCESSES/$WORKER_PROCESSES/g" -e "s/\$IS_THREAD/$IS_THREAD/g"  uwsgi-template.ini > $PROJECT_DIR/confs/uwsgi.ini
 ln -s $PROJECT_DIR/confs/uwsgi.ini /etc/uwsgi/vassals/$PROJECT_NAME.ini
 fi
-
-#set supervisor
-echo "Do you wish to set supervisor for $PROJECT_NAME ? (y/n)"
-read spyn
 if [[ ($spyn == "y" || $spyn == "") ]]; then
-	echo "setting supervisor configurations ..."
-	echo "Do you have beat for $PROJECT_NAME ? (y/n)"
-	read btyn
-	echo "setting worker and beat configurations ..."
-	if [[ ($btyn == "y" || $btyn == "") ]]; then
-		echo "[group:$PROJECT_NAME]
-programs=$PROJECT_NAME-beat,$PROJECT_NAME-worker" >> $PROJECT_DIR/confs/supervisor.conf
-  else
-    echo "[group:$PROJECT_NAME]
-programs=$PROJECT_NAME-worker" >> $PROJECT_DIR/confs/supervisor.conf
-	fi
-
-	sed -e "s/\$PROJECT_NAME/$PROJECT_NAME/g" -e "s/\$OWNER_USER/$OWNER_USER/g"  supervisor-worker-template.conf >> $PROJECT_DIR/confs/supervisor.conf
-	
-	if [[ ($btyn == "y" || $btyn == "") ]]; then
-		sed -e "s/\$PROJECT_NAME/$PROJECT_NAME/g" -e "s/\$OWNER_USER/$OWNER_USER/g"  supervisor-beat-template.conf >> $PROJECT_DIR/confs/supervisor.conf	
-	fi
-
-	ln -s $PROJECT_DIR/confs/supervisor.conf /etc/supervisor/conf.d/$PROJECT_NAME.conf
-		
 	supervisorctl reread
 	supervisorctl update
-
-fi
-
-echo "creating main database ..."
-echo "enter [user] [host] [port](optional): "
-
-read DB_HOST_USER HOST PORT
-
-if [ -z "$PORT" ]; then
-	PORT=5432
-fi
-
-DB_USER_CREATE=${PROJECT_NAME}_user$(awk -v min=1 -v max=1000 'BEGIN{srand(); print int(min+rand()*(max-min+1))}')
-DB_USER_CREATE_PASSWORD=$(randomPassword)
-psql -U "$DB_HOST_USER" -h "$HOST" -p "$PORT" -W <<EOF
-CREATE DATABASE $PROJECT_NAME WITH ENCODING 'UTF8';
-CREATE USER $DB_USER_CREATE WITH PASSWORD '$DB_USER_CREATE_PASSWORD';
-GRANT ALL PRIVILEGES ON DATABASE $PROJECT_NAME TO $DB_USER_CREATE;
-
-
-EOF
-echo "USER OF THE DATABASE $PROJECT_NAME IS $DB_USER_CREATE WITH PASSWORD $DB_USER_CREATE_PASSWORD"
-echo "DB_USER=$DB_USER_CREATE
-DB_PASSWORD=$DB_USER_CREATE_PASSWORD
-DB_NAME=$PROJECT_NAME
-DB_HOST=$HOST
-DB_PORT=$PORT" >> $PROJECT_DIR/project/.env
-echo "done"
-
-if [[ ($spyn == "y" || $spyn == "") ]]; then
-	echo "creating broker vhost ..."
-	echo "enter [username] [password] [host] [port](optional): "
-	read RBMQ_USERNAME RBMQ_PASSWORD RBMQ_HOST RBMQ_PORT
-	if [ -z "$RBMQ_PORT" ]; then
-	RBMQ_PORT=15672
-  fi
-
-	RBMQ_USER_CREATE=${PROJECT_NAME}_user$(awk -v min=1 -v max=1000 'BEGIN{srand(); print int(min+rand()*(max-min+1))}')
-	RBMQ_USER_CREATE_PASSWORD=$(randomPassword)
-	curl -u "$RBMQ_USERNAME":"$RBMQ_PASSWORD" -X PUT http://"$RBMQ_HOST":"$RBMQ_PORT"/api/vhosts/"$PROJECT_NAME"
-
-	
-	curl -i -u "$RBMQ_USERNAME":"$RBMQ_PASSWORD" -H "content-type:application/json"     -XPUT -d'{"password":"'$RBMQ_USER_CREATE_PASSWORD'","tags":"monitoring"}'     http://"$RBMQ_HOST":"$RBMQ_PORT"/api/users/"$RBMQ_USER_CREATE"
-        curl -i -u "$RBMQ_USERNAME":"$RBMQ_PASSWORD" -H "content-type:application/json"     -XPUT -d'{"configure":".*","write":".*","read":".*"}'     http://"$RBMQ_HOST":"$RBMQ_PORT"/api/permissions/"$PROJECT_NAME"/"$RBMQ_USER_CREATE"
-
-	echo "USER OF THE RBMQ $PROJECT_NAME IS $RBMQ_USER_CREATE WITH PASSWORD $RBMQ_USER_CREATE_PASSWORD"
-
-	echo "CELERY_USER=$RBMQ_USER_CREATE
-CELERY_PASS=$RBMQ_USER_CREATE_PASSWORD
-CELERY_HOST=$RBMQ_HOST:$RBMQ_PORT/$PROJECT_NAME" >> $PROJECT_DIR/project/.env
-
-
-fi
-
+fi	
 if [ ! -z "$OWNER_USER" ]; then
 	echo "Changing the owner of $PROJECT_DIR to $OWNER_USER"
 	chown -R $OWNER_USER: $PROJECT_DIR
